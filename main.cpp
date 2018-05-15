4c9d38620497ba6546d141624dac21fc8ca67615
Larry Lai <dppss91102@gmail.com>
2018-05-15 21:08:38 +0800

    All Accepted (10/10)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
-#define int_max numeric_limits<int>::max()
+#define int_max 2147483647
 
 int** makeMatrix(int n);
-int** copyMatrix(int n, int **copy);
-void showPath(int** cost, int** pts, int i, int j);
+void showPath(int** next, int i, int j);
 
 int main() {
     int n;
     cin >> n;
 
-    int **cost, **sho;
-    cost = makeMatrix(n);
-    sho = copyMatrix(n, cost);
+    int **sho;
+    sho = makeMatrix(n);
 
-    auto **midPts =  (int **) malloc(sizeof(int*) * n);
-    for (int i = 0; i < n; i++)
-        midPts[i] = (int *) malloc(sizeof(int) * n);
+    auto **next =  (int **) malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; i++) {
+        next[i] = (int *) malloc(sizeof(int) * n);
+        for (int j = 0; j < n; j++)
+            next[i][j] = j;
+    }
 
     for (int k = 0; k < n; ++k)
         for (int i = 0; i < n; ++i)
@@ -24,16 +25,19 @@ int main() {
                 int m = (sho[i][k] == int_max || sho[k][j] == int_max)?int_max:(sho[i][k] + sho[k][j]);
                 if (m < sho[i][j]) {
                     sho[i][j] = m;
-                    midPts[i][j] = k;
+                    next[i][j] = next[i][k];
+                } else if (m == sho[i][j]) {
+                    if (next[i][j] > next[i][k] && k != i)
+                        next[i][j] = next[i][k];
                 }
             }
 
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             if (sho[i][j] != 0 && sho[i][j] != int_max) {
-                cout << "Path(" << i << "," << j << "):" << i << "->";
-                showPath(cost, midPts, i, j);
-                cout << j << endl << "Cost:" << sho[i][j] << endl;
+                cout << "Path(" << i << "," << j << "):" << i;
+                showPath(next, i, j);
+                cout << endl << "Cost:" << sho[i][j] << endl;
             }
 
     return 0;
@@ -52,19 +56,9 @@ int** makeMatrix(int n){
 
     return temp;
 }
-int** copyMatrix(int n, int **copy){
-    auto **temp = (int **) malloc(sizeof(int*) * n);
-    for (int i = 0; i < n; i++){
-        temp[i] = (int *) malloc(sizeof(int) * n);
-        for (int j = 0; j < n; j++)
-            temp[i][j] = copy[i][j];
-    }
-
-    return temp;
-}
-void showPath(int** cost, int** pts, int i, int j){
-    if (cost[i][j] == int_max){
-        showPath(cost, pts, i, pts[i][j]);
-        cout << pts[i][j] << "->";
+void showPath(int** next, int i, int j){
+    if (i != j){
+        cout << "->" << next[i][j];
+        showPath(next, next[i][j], j);
     }
 }
